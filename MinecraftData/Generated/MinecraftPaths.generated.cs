e6f0cfd5681e14256ce79d4c7620c62f342fe678
TitleHHHH <TitleHHHH@outlook.com>
2025-09-26 21:47:33 +0500

    add helper lib

---
+++ MinecraftData/Generated/MinecraftPaths.generated.cs
@@ -0,0 +1,6 @@
+namespace MinecraftData;
+
+public static partial class MinecraftPaths
+{
+    private const string DataDir = @"C:\Users\Title\RiderProjects\MinecraftDataFSharp\minecraft-data\data\";
+}

21c522b4d100e0fcc549b84d6b2ee23975e37cb9
TitleHHHH <TitleHHHH@outlook.com>
2025-09-28 23:01:38 +0500

    update gitignore

--- MinecraftData/Generated/MinecraftPaths.generated.cs
+++
@@ -1,6 +0,0 @@
-namespace MinecraftData;
-
-public static partial class MinecraftPaths
-{
-    private const string DataDir = @"C:\Users\Title\RiderProjects\MinecraftDataFSharp\minecraft-data\data\";
-}
